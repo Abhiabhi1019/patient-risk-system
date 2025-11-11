7ec05453d13b690a013c180cd7537d35d09e30df
Abhijith CM <143163719+Abhiabhi1019@users.noreply.github.com>
2025-11-11 20:22:59 +0530

    Modify streamlit_app.py to include project root
    add few lines

--- src/dashboard/streamlit_app.py
+++ src/dashboard/streamlit_app.py
@@ -1,3 +1,10 @@
+import sys
+import os
+
+# Add project root to sys.path
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
+if project_root not in sys.path:
+    sys.path.append(project_root)
 import streamlit as st
 import pandas as pd
 import altair as alt
